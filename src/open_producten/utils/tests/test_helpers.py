79bb0ef5f87149e986ef357eee6952139e9eec8b
Floris272 <floris@maykinmedia.nl>
2024-08-20 10:02:07 +0200

    Change formset data order

--- src/open_producten/utils/tests/test_helpers.py
+++ src/open_producten/utils/tests/test_helpers.py
@@ -1,8 +1,8 @@
 def build_formset_data(form_name: str, *forms: dict, extra: dict | None = None):
-    data = {
+    data = (extra if extra else {}) | {
         f"{form_name}-TOTAL_FORMS": len(forms),
         f"{form_name}-INITIAL_FORMS": "0",
-    } | (extra if extra else {})
+    }
 
     for i, form in enumerate(forms):
         for key in form:
