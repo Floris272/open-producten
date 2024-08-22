b52b65f8254c09721ccc87e881e2d1116ffa8c35
Floris272 <floris@maykinmedia.nl>
2024-08-22 15:54:58 +0200

    Add condition, tag & tag type api tests

---
+++ src/open_producten/producttypes/tests/api/test_tag_type.py
@@ -0,0 +1,71 @@
+from django.forms import model_to_dict
+
+from open_producten.producttypes.models import TagType
+from open_producten.utils.tests.test_cases import BaseApiTestCase
+
+from ..factories import TagTypeFactory
+
+
+def tag_type_to_dict(tag):
+    tag_dict = model_to_dict(tag) | {"id": str(tag.id)}
+    return tag_dict
+
+
+class TestProductTypeTagType(BaseApiTestCase):
+
+    def setUp(self):
+        self.data = {"name": "test tag_type"}
+        self.endpoint = "/api/v1/tagtypes/"
+
+    def create_tag(self):
+        return TagTypeFactory.create()
+
+    def test_create_tag(self):
+        response = self.post(self.data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(TagType.objects.count(), 1)
+        self.assertEqual(TagType.objects.first().name, "test tag_type")
+
+    def test_update_tag(self):
+        tag_type = self.create_tag()
+
+        data = self.data | {"name": "updated"}
+        response = self.put(tag_type.id, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(TagType.objects.count(), 1)
+        self.assertEqual(TagType.objects.first().name, "updated")
+
+    def test_partial_update_tag(self):
+        tag_type = self.create_tag()
+
+        data = {"name": "updated"}
+        response = self.patch(tag_type.id, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(TagType.objects.count(), 1)
+        self.assertEqual(TagType.objects.first().name, "updated")
+
+    def test_read_tag_types(self):
+        tag_type = self.create_tag()
+
+        response = self.get()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [tag_type_to_dict(tag_type)])
+
+    def test_read_tag(self):
+        tag_type = self.create_tag()
+
+        response = self.get(tag_type.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, tag_type_to_dict(tag_type))
+
+    def test_delete_tag(self):
+        tag_type = self.create_tag()
+        response = self.delete(tag_type.id)
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(TagType.objects.count(), 0)
