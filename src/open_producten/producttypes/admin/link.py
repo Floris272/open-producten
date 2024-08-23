8e49b7a55382d0cb7de4e9686ce20527920d1317
Floris272 <floris@maykinmedia.nl>
2024-08-23 17:58:07 +0200

    Add link admin get_queryset

--- src/open_producten/producttypes/admin/link.py
+++ src/open_producten/producttypes/admin/link.py
@@ -12,3 +12,6 @@ class LinkInline(admin.TabularInline):
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
     list_display = ("product_type", "name", "url")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('product_type')
